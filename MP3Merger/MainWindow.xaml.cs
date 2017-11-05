ff5d8205a1134d7e9f53068fdb17444114e66930
jl_leonr <jl_leonr@yahoo.com>
2017-11-05 14:36:47 -0400

    Removed code-behind.

--- MP3Merger/MainWindow.xaml.cs
+++ MP3Merger/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Win32;
-using MP3Merger1.ViewModel;
-using System.Windows;
+﻿using System.Windows;
 
 namespace MP3Merger
 {
@@ -14,50 +12,6 @@ namespace MP3Merger
         {
             
             InitializeComponent();
-
-        }
-        
-
-        private void btn1_Click(object sender, RoutedEventArgs e)
-        {
-            
-
-        }
-
-        private void btn2_Click(object sender, RoutedEventArgs e)
-        {
-
-            
-        }
-
-        private void btn3_Click(object sender, RoutedEventArgs e)
-        {
-            
-
-        }
-
-        public void btn4_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void mp3Merger()
-        {
-           
-        }
-
-        public static void convertWavStreamToMp3File(string outputFile, byte[] waveFile)
-        {
-
-        }
-
-        public void saveToDisk(byte[] mp3File, string destinationFile)
-        {
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
 
         }
 
