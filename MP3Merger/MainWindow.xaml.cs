b1e3222fcc9f782968050305503322af27d14a16
jl_leonr <jl_leonr@yahoo.com>
2017-11-05 14:35:58 -0400

    Renamed MP3Merger2 to MP3Merger

---
+++ MP3Merger/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Win32;
+using MP3Merger1.ViewModel;
+using System.Windows;
+
+namespace MP3Merger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+
+        }
+        
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        public void btn4_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void mp3Merger()
+        {
+           
+        }
+
+        public static void convertWavStreamToMp3File(string outputFile, byte[] waveFile)
+        {
+
+        }
+
+        public void saveToDisk(byte[] mp3File, string destinationFile)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
