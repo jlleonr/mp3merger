93ceb4d77da1ad19e5ffb60c05604bccb01c5156
jl_leonr <jl_leonr@yahoo.com>
2017-11-05 15:15:36 -0400

    Refactored the merging method into a separate service.

--- MP3Merger/ViewModel/MP3FilesViewModel.cs
+++ MP3Merger/ViewModel/MP3FilesViewModel.cs
@@ -11,10 +11,7 @@
 
 using Microsoft.Win32;
 using MP3Merger1.Model;
-using NAudio.Lame;
-using NAudio.Wave;
-using System;
-using System.IO;
+using MP3Merger2.Services;
 using System.Windows;
 
 namespace MP3Merger1.ViewModel
@@ -27,8 +24,21 @@ namespace MP3Merger1.ViewModel
         private const string initF1 = "Path to file #1";
         private const string initF2 = "Path to file #2";
         private const string initOutDir = "Output Directory";
+        private OpenFileDialog openFileDialog;
 
         public MP3FilesModel MP3Files { get; private set; }
+
+        private bool ValidPaths()
+        {
+            if ((MP3Files.FileName1 == MP3Files.FileName2)
+                || (MP3Files.FileName1 == MP3Files.OutputDirectory)
+                || (MP3Files.FileName2 == MP3Files.OutputDirectory))
+            {
+                MessageBox.Show("The file names or directories cannot be repeated!");
+                return false;
+            }
+            return true;
+        }
 
         /// <summary>
         /// Set initial values to the Model properties,
@@ -43,28 +53,48 @@ namespace MP3Merger1.ViewModel
                 FileName2 = initF2,
                 OutputDirectory = initOutDir
             };
+
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3";
         }
 
+        /// <summary>
+        /// Set path for first mp3 file.
+        /// </summary>
         public void OnSetFile1()
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Filter = "MP3 files (*.mp3)|*.mp3";
-            if (openFileDialog1.ShowDialog() == true)
+            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            //openFileDialog1.Filter = "MP3 files (*.mp3)|*.mp3";
+            if (openFileDialog.ShowDialog() == true)
             {
-                MP3Files.FileName1 = openFileDialog1.FileName;
+                MP3Files.FileName1 = openFileDialog.FileName;
+
+                if (!ValidPaths())
+                {
+                    MP3Files.FileName1 = initF1;
+                }
             }
         }
 
+        /// <summary>
+        /// Set path for second mp3 file.
+        /// </summary>
         public void OnSetFile2()
         {
-            OpenFileDialog openFileDialog2 = new OpenFileDialog();
-            openFileDialog2.Filter = "MP3 files (*.mp3)|*.mp3";
-            if (openFileDialog2.ShowDialog() == true)
+            if (openFileDialog.ShowDialog() == true)
             {
-                MP3Files.FileName2 = openFileDialog2.FileName;
+                MP3Files.FileName2 = openFileDialog.FileName;
+
+                if (!ValidPaths())
+                {
+                    MP3Files.FileName2 = initF2;
+                }
             }
         }
 
+        /// <summary>
+        /// Set output directory.
+        /// </summary>
         public void OnSetOutDir()
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
@@ -74,10 +104,18 @@ namespace MP3Merger1.ViewModel
             if (saveFileDialog.ShowDialog() == true)
             {
                 MP3Files.OutputDirectory = saveFileDialog.FileName;
+
+                if (!ValidPaths())
+                {
+                    MP3Files.OutputDirectory = initOutDir;
+                }
             }
         }
 
-        public void OnMix()
+        /// <summary>
+        /// Merge the selected files.
+        /// </summary>
+        public void OnMerge()
         {
 
             if ((string.IsNullOrEmpty(MP3Files.FileName1) || MP3Files.FileName1.Equals(initF1))
@@ -87,81 +125,8 @@ namespace MP3Merger1.ViewModel
                 MessageBox.Show("Select two mp3 files and an output directory first.");
                 return;
             }
-            mp3Merger();
-        }
 
-        private void mp3Merger()
-        {
-            var fileA = new AudioFileReader(MP3Files.FileName1);
-
-            // Calculate our buffer size, since we're normalizing our samples to floats
-            // we'll need to account for that by dividing the file's audio byte count
-            // by its bit depth / 8.
-            var bufferA = new float[fileA.Length / (fileA.WaveFormat.BitsPerSample / 8)];
-
-            // Now let's populate our buffer with samples.
-            fileA.Read(bufferA, 0, bufferA.Length);
-
-            // Do it all over again for the other file.
-            var fileB = new AudioFileReader(MP3Files.FileName2);
-            var bufferB = new float[fileB.Length / (fileB.WaveFormat.BitsPerSample / 8)];
-            fileB.Read(bufferB, 0, bufferB.Length);
-
-            // Calculate the largest file
-            var maxLen = (long)Math.Max(bufferA.Length, bufferB.Length);
-            var final = new byte[maxLen];
-
-            // For now, mix data to a wav file.
-            using (MemoryStream ms = new MemoryStream())
-            {
-                var writer = new WaveFileWriter(ms, fileA.WaveFormat);
-
-                for (var i = 0; i < maxLen; i++)
-                {
-                    float a, b;
-
-                    if (i < bufferA.Length)
-                    {
-                        // Reduce the amplitude of the sample by 2
-                        // to avoid clipping.
-                        a = bufferA[i];// / 2;
-                    }
-                    else
-                    {
-                        a = 0;
-                    }
-
-                    if (i < bufferB.Length)
-                    {
-                        b = bufferB[i];// / 2;
-                    }
-                    else
-                    {
-                        b = 0;
-                    }
-                    writer.WriteSample(a + b);
-                    writer.Flush();
-                }
-                ms.Seek(0, SeekOrigin.Begin);
-                final = ms.ToArray();             
-            }
-            convertWavStreamToMp3File(MP3Files.OutputDirectory, final);
-            MessageBox.Show("Finished");
-        }
-
-        private static void convertWavStreamToMp3File(string outputFile, byte[] waveFile)
-        {
-            using (var ms = new MemoryStream(waveFile))
-            using (var wfr = new WaveFileReader(ms))
-            using (var wtr = new LameMP3FileWriter(outputFile, wfr.WaveFormat, LAMEPreset.VBR_100))
-            {
-                wfr.CopyTo(wtr);
-            }
-        }
-
-        private void saveToDisk(byte[] mp3File, string destinationFile)
-        {
-            File.WriteAllBytes(destinationFile, mp3File);
+            MergeService.Mp3Merge(MP3Files.FileName1, MP3Files.FileName2, MP3Files.OutputDirectory);
         }
 
     }
